a1103e7f7718c2bda462ce4d30feb4fb3390afda
PyFactory <afauconnet@gmail.com>
2017-12-23 01:32:47 +0000

    Removed public-ip, server now uses process.env.URL

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
 const path = require('path')
 global.appRoot = path.resolve(__dirname)
-
-const publicIp = require('public-ip')
-publicIp.v4().then(ip => {
-	global.appUrl = "http://"+ip
-  const server = require('echoplay-server')
-})
+const server = require('echoplay-server')
