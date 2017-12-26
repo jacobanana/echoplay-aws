c7ebd3c370a06cca64116ef46e2c897494c7cfc4
PyFactory <afauconnet@gmail.com>
2017-12-26 00:41:33 +0000

    server update

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const path = require('path')
 const {EchoPlayServer} = require('echoplay-server')
 
-var server = new EchoPlayServer(path.resolve(__dirname), process.env.URL, process.env.PORT)
+var server = new EchoPlayServer(path.resolve(__dirname), process.env.PORT, process.env.URL)
 server.start()
